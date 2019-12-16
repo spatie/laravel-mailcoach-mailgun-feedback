f76538669165e584f682575f423c985010e992f6
freek <freek@spatie.be>
2019-12-16 09:11:21 +0100

    wip

--- src/ProcessMailgunWebhookJob.php
+++ src/ProcessMailgunWebhookJob.php
@@ -4,10 +4,18 @@ namespace Spatie\MailcoachMailgunFeedback;
 
 use Illuminate\Support\Arr;
 use Spatie\Mailcoach\Models\Send;
+use Spatie\WebhookClient\Models\WebhookCall;
 use Spatie\WebhookClient\ProcessWebhookJob;
 
 class ProcessMailgunWebhookJob extends ProcessWebhookJob
 {
+    public function __construct(WebhookCall $webhookCall)
+    {
+        parent::__construct($webhookCall);
+
+        $this->queue = config('mailcoach.perform_on_queue.process_feedback_job');
+    }
+
     public function handle()
     {
         $payload = $this->webhookCall->payload;
