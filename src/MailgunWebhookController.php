1c5e784994f45b097683e48303c1b27662af2b2b
riasvdv <rias@spatie.be>
2022-11-09 10:55:10 +0000

    Add v5 to branches

--- src/MailgunWebhookController.php
+++ src/MailgunWebhookController.php
@@ -3,43 +3,16 @@
 namespace Spatie\MailcoachMailgunFeedback;
 
 use Illuminate\Http\Request;
-use Spatie\Mailcoach\Domain\Shared\Traits\UsesMailcoachModels;
-use Spatie\WebhookClient\Exceptions\InvalidWebhookSignature;
 use Spatie\WebhookClient\WebhookProcessor;
 
 class MailgunWebhookController
 {
-    use UsesMailcoachModels;
-
     public function __invoke(Request $request)
     {
-        $this->registerMailerConfig($request->route('mailer'));
-
         $webhookConfig = MailgunWebhookConfig::get();
 
-        try {
-            (new WebhookProcessor($request, $webhookConfig))->process();
-        } catch (InvalidWebhookSignature $exception) {
-            report($exception);
-
-            return response()->json(['message' => $exception->getMessage()], 406);
-        }
+        (new WebhookProcessor($request, $webhookConfig))->process();
 
         return response()->json(['message' => 'ok']);
-    }
-
-    public function registerMailerConfig(?string $mailer): void
-    {
-        if (! $mailer) {
-            return;
-        }
-
-        $mailer = cache()->remember(
-            "mailcoach-mailer-{$mailer}",
-            now()->addMinute(),
-            fn () => self::getMailerClass()::findByConfigKeyName($mailer),
-        );
-
-        $mailer?->registerConfigValues();
     }
 }
